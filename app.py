71f1ed2f6756a13bc0a2fdd23f536e6099df401c
hessdale <hessdale96@gmail.com>
2024-04-09 22:47:51 -0700

    added endpoint for submitting contact form to server

--- app.py
+++ app.py
@@ -202,6 +202,25 @@ def post_portrait_image():
     except ValueError:
         print("value error, try again")       
 
+@app.post("api/contact-form")
+def post_contact_form():
+    try:
+        error = dbhelper.check_endpoint_info(request.form,['FirstName','LastName','Email','Note'])
+        if(error != None):
+            return make_response(jsonify(error),400)
+        results = dbhelper.run_procedure("call post_contact_form(?,?,?)",[request.form['FirstName'],request.form['LastName'],request.form['Email'],request.form['Note'],])
+        if(type(results) == list):
+            return make_response(jsonify(results),200)
+        else:
+            return make_response("sorry something went wrong",500)
+    # some except blocks with possible errors
+    except TypeError:
+        print("invalid input type, try again.")
+    except UnboundLocalError:
+        print("coding error")
+    except ValueError:
+        print("value error, try again")    
+
 if(dbcreds.production_mode == True):
     print("Running Production Mode")
     import bjoern #type: ignore
